662b89a1eb5c43016139f870ef54b49a0a579c8c
slash0slash <saiti.po4ta@yandex.ru>
2023-12-02 10:07:10 +0300

    new files

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,9 @@
+from .pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def test_guest_can_go_to_login_page(driver):
-    driver = webdriver.Chrome()
     link = "http://selenium1py.pythonanywhere.com/"
-    driver.get(link)
-    login_link = driver.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(driver, link)
+    page.open()
+    page.go_to_login_page()
