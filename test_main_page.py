83e738998618f0ad414efe8c46fd41c70a332e43
slash0slash <saiti.po4ta@yandex.ru>
2023-12-02 16:44:33 +0300

    my new files

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,8 @@ def test_guest_can_go_to_login_page(driver):
     page = MainPage(driver, link)
     page.open()
     page.go_to_login_page()
+def test_guest_should_see_login_link(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(driver, link)
+    page.open()
+    page.should_be_login_link()
